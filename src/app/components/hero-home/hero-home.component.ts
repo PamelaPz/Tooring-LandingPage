73761d5bc1591245a4d6c2d1e18e4e5587440aa4
PamelaPz <jpamelapz02@gmail.com>
2020-08-29 14:24:23 -0500

    comenzando proyecto landing page de tooring

---
+++ src/app/components/hero-home/hero-home.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'hero-home',
+  templateUrl: './hero-home.component.html',
+  styleUrls: ['./hero-home.component.sass']
+})
+export class HeroHomeComponent {
+
+  constructor() { }
+
+}
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'NavBar_Home',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.sass']
+})
+export class NavbarComponent {
+
+  constructor() { }
+
+}
