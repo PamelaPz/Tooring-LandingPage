adfab380e84256003a8edb1470004ba4504f3818
PamelaPz <jpamelapz02@gmail.com>
2020-08-31 12:22:22 -0500

    new component of solutions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.sass']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
+  ngOnInit(): void {
+    $(document).ready(function(){
+      //Creamos un evento click para el enlace
+      $(".ancla").click(function(evento){
+        //Anulamos la funcionalidad por defecto del evento
+        evento.preventDefault();
+        //Creamos el string del enlace ancla
+        var codigo = "#" + $(this).data("ancla");
+        //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+        $("html,body").animate({scrollTop: $(codigo).offset().top-60}, 2000);
+      });
+    });
+  }
+
   title = 'Tooring-LandingPage';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,24 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeroHomeComponent } from './components/hero-home/hero-home.component';
 import { SomosHomeComponent } from './components/somos-home/somos-home.component';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { DiagnosticHomeComponent } from './components/diagnostic-home/diagnostic-home.component';
+import { SolutionsComponent } from './components/solutions/solutions.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     HeroHomeComponent,
-    SomosHomeComponent
+    SomosHomeComponent,
+    DiagnosticHomeComponent,
+    SolutionsComponent
   ],
   imports: [
     BrowserModule,
