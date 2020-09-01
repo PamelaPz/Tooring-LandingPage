ab81302ab854734523a890eec950138e5af08661
PamelaPz <jpamelapz02@gmail.com>
2020-09-01 12:59:26 -0500

    new component design

---
+++ src/app/components/design-home/design-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'design-home',
+  templateUrl: './design-home.component.html',
+  styleUrls: ['./design-home.component.sass']
+})
+export class DesignHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
