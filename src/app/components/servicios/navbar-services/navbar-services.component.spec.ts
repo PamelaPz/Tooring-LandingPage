10d39ed4611fe2b4f94881c2910d9449df6a1327
PamelaPz <jpamelapz02@gmail.com>
2020-09-09 14:09:10 -0500

    navbar de servicios

---
+++ src/app/components/servicios/navbar-services/navbar-services.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { NavbarServicesComponent } from './navbar-services.component';
+
+describe('NavbarServicesComponent', () => {
+  let component: NavbarServicesComponent;
+  let fixture: ComponentFixture<NavbarServicesComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ NavbarServicesComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(NavbarServicesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/components/servicios/navbar-services/navbar-services.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'navbar-services',
+  templateUrl: './navbar-services.component.html',
+  styleUrls: ['./navbar-services.component.sass']
+})
+export class NavbarServicesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
