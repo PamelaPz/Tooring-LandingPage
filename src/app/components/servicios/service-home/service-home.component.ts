aee98ddad16ac402dd0ed15896c4db347100e4d2
PamelaPz <jpamelapz02@gmail.com>
2020-09-09 14:09:52 -0500

    servicios principal

---
+++ src/app/components/servicios/service-home/service-home.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-service-home',
+  templateUrl: './service-home.component.html',
+  styleUrls: ['./service-home.component.sass']
+})
+export class ServiceHomeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
