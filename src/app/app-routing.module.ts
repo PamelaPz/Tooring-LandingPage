44e6bfb8be320734bb0b501f9fa911065f093785
PamelaPz <jpamelapz02@gmail.com>
2020-09-11 09:47:47 -0500

    navbar rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,14 @@ import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ServiciosComponent } from './components/servicios/servicios.component';
-import { WebComponent } from './components/servicios/service-home/web/web.component';
+import { ServiceHomeComponent } from './components/servicios/service-home/service-home.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full', },
   { path: 'home', component: HomeComponent },
   { path: 'servicios', component: ServiciosComponent, children: 
     [ 
-      { path: 'web', component: WebComponent },
+      { path: 'web', component: ServiceHomeComponent },
     ]
   },
 ];
