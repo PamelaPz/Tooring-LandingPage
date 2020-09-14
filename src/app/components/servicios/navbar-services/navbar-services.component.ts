3b4227491c903673b7750334c8102e6e63cfa860
PamelaPz <jpamelapz02@gmail.com>
2020-09-14 12:41:19 -0500

    navbar

--- src/app/components/servicios/navbar-services/navbar-services.component.ts
+++ src/app/components/servicios/navbar-services/navbar-services.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'navbar-services',
@@ -10,6 +11,23 @@ export class NavbarServicesComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    $(document).ready(function(){
+      //Creamos un evento click para el enlace
+      $(".ancla").click(function(evento){
+        //Anulamos la funcionalidad por defecto del evento
+        evento.preventDefault();
+        //Creamos el string del enlace ancla
+        var codigo = "#" + $(this).data("ancla");
+
+        if (codigo == "#footer") {
+          //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+          $("html,body").animate({scrollTop: $(codigo).offset().top}, 2000);
+        } else {
+          //Funcionalidad de scroll lento para el enlace ancla en 3 segundos
+          $("html,body").animate({scrollTop: $(codigo).offset().top-30}, 2000);
+        }
+      });
+    });
   }
 
 }
